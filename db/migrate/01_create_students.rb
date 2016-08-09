14a7058508880be480440797de95da6847f0d042
Troy Chryssos <github email address>
2016-08-09 09:27:27 -0400

    Wrote new migration files and methods to add and change data to a table.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
 class CreateStudents < ActiveRecord::Migration
+  #
+  # def create_table
+  #   create_table(:students)
+  # end
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
